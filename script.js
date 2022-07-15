a38db42b4b9d2c1ca9326de085e87bd7b5a11cde
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-07-15 01:46:25 -0700

    index.html file update to remove text options, script.js logic errors found, getPrompt function logic error fix

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 // Assignment code here
-// Declare user choice as global scope
-let choiceArr = [];
 
-// Set default password character length
-var charLength = 8;
 // Assign values in arrays for password
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G','H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P','Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g','h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var numberArr = ['1', '2', '3', '4', '5', '6', '7','8', '9', '0'];
-var specialChar = ['!', '@', '#', '$', '%', '^', '&','*', '(', ')', '-', '+', ':', '<', '>', '?'];
+let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G','H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P','Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g','h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let numberArr = ['1', '2', '3', '4', '5', '6', '7','8', '9', '0'];
+let specialChar = ['!', '@', '#', '$', '%', '^', '&','*', '(', ')', '-', '+', ':', '<', '>', '?'];
 
 // 1. Prompt the user with a series of password criteria:
 function getPrompts() {
   // reset choiceArr to be empty array
   choiceArr = [];
 
-  charLength = parseInt(prompt("Please pick a character length between 8 < 128 characters!"));
+  charLength = (prompt("Please pick a character length between 8 < 128 characters!"));
   // parseInt added to convert to integer or NaN 
    if(isNaN(charLength) || charLength < 8 || charLength > 128) {
     //  Verify length conditions are acceptable
@@ -24,7 +20,7 @@ function getPrompts() {
      return false;
    }
   //  proceed to add Uppercase choice array
-   if(confirm("Would you like to include Uppercase Letters in your password?")) {
+  if(confirm("Would you like to include Uppercase Letters in your password?")) {
     choiceArr = choiceArr.concat(upperCase);
    }
   //  proceed to add Lowercase choice array
@@ -38,10 +34,16 @@ function getPrompts() {
    //  proceed to add Special Characters choice array
    if(confirm("Would you like to include Special Characters in your password?")) {
     choiceArr = choiceArr.concat(specialChar);
-    // if all conditions met successively, return true to proceed to truePrompt variable
-   return true;
   }
-}
+  //  
+  if (choiceArr) {
+    return false;
+    alert("You must pick at least one option! Please try again")
+  }
+  // if all conditions met successively, return true to proceed to truePrompt variable
+      return true;
+};
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -54,7 +56,7 @@ function writePassword() {
 // Verify acceptable prompt criteria return true
   var truePrompts = getPrompts();
   // if getPrompts is true, proceed to generatePassword function
-    if (truePrompts) {
+    if (truePrompts === true) {
       var uniquePassword = generatePassword();
       var passwordText = document.querySelector("#password");
       passwordText.value = uniquePassword;
@@ -68,7 +70,7 @@ function writePassword() {
 function generatePassword() {
   // test click to console log
   // console.log("Generate Password Button is Clicked!");
-  var uniquePassword = "";
+  let uniquePassword = "";
   // based on the getPrompt user input choiceArr
   for(var i = 0; i < charLength; i++ ) {
     // incrementally add random charArr index up to max charLength
