dcb4a1691e0ed22397116c7de9cdd2f4da97b459
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-07-15 02:52:22 -0700

    validation of user input null error

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Assignment code here
 
 // Assign const variables in arrays for password char set
-const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G','H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P','Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g','h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const numberArr = ['1', '2', '3', '4', '5', '6', '7','8', '9', '0'];
-const specialChar = ['!', '@', '#', '$', '%', '^', '&','*', '(', ')', '-', '+', ':', '<', '>', '?'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G','H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P','Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g','h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numberArr = ['1', '2', '3', '4', '5', '6', '7','8', '9', '0'];
+var specialChar = ['!', '@', '#', '$', '%', '^', '&','*', '(', ')', '-', '+', ':', '<', '>', '?'];
 
 // 1. Prompt the user with a series of password criteria:
 function getPrompts() {
@@ -15,10 +15,12 @@ function getPrompts() {
   // parseInt added to convert to integer or NaN 
    if(charLength < 8 || charLength > 128) {
     //  Verify length conditions are acceptable
-     return("You must choose a number between 8 < 128! Please pick again!");
+     alert("You must choose a number between 8 < 128! Please pick again!")
+     return false;
     //  Verify if user input is Not a Number
    }  else if (isNaN(charLength)) {
-    charLength = prompt("Your choice is not a Number!");
+    alert("Your choice is not a Number!")
+    return false;
    }
   //  proceed to add Uppercase choice array
   includeUpperCase = confirm("Would you like to include Uppercase Letters in your password?");
@@ -31,23 +33,26 @@ function getPrompts() {
     choiceArr = choiceArr.concat(lowerCase);
    }
    //  proceed to add Number choice array
-   if(confirm("Would you like to include Numbers in your password?")) {
+   includeNumbers = confirm("Would you like to include Numbers in your password?");
+  if (includeNumbers){
     choiceArr = choiceArr.concat(numberArr);
    }
    //  proceed to add Special Characters choice array
-   if(confirm("Would you like to include Special Characters in your password?")) {
+   includeSpecialChar = confirm("Would you like to include Special Characters in your password?");
+   if (includeSpecialChar){
     choiceArr = choiceArr.concat(specialChar);
   }
   //  check if user does not select any options to prevent password to be generated undefined
-  if (upperCase === false && upperCase === false && numberArr === false && specialChar === false) {
+  else if (upperCase === false && upperCase === false && numberArr === false && specialChar === false) {
     // throw alert error message and stop function
-    return ("You must pick at least one option! Please try again");
+    alert ("You must pick at least one option! Please try again")
+    return false;
   }
 };
 
 
 // Get references to the #generate element
-const generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
@@ -55,9 +60,9 @@ generateBtn.addEventListener("click", writePassword);
 // Write password to the #password input
 function writePassword() {
 // Verify acceptable prompt criteria return true
-  var truePrompts = getPrompts();
+  var truePrompts = getPrompts() 
   // if getPrompts is true, proceed to generatePassword function
-    if (truePrompts) {
+    if (truePrompts === true) {
       var uniquePassword = generatePassword();
       var passwordText = document.querySelector("#password");
       passwordText.value = uniquePassword;
@@ -68,7 +73,7 @@ function writePassword() {
 function generatePassword() {
   // test click to console log
   // console.log("Generate Password Button is Clicked!");
-  let uniquePassword = "";
+  var uniquePassword = "";
   // based on the getPrompt user input choiceArr
   for(var i = 0; i < charLength; i++ ) {
     // incrementally add random charArr index up to max charLength
